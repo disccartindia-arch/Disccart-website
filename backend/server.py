fd464e12ec8c312d94ebb6137c920b8e30a31b36
disccart <disccartindia@gmail.com>
2026-04-02 10:59:19 +0000

    fix cors issue

--- backend/server.py
+++ backend/server.py
@@ -36,6 +36,22 @@ db = client["disccart"]
 
 # App
 app = FastAPI()
+from fastapi.middleware.cors import CORSMiddleware
+
+origins = [
+    "https://disccart.in",
+    "http://localhost:5173",
+    "http://localhost:3000"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 api_router = APIRouter()
 
 # Middleware
